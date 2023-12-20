e7848d5e76fa3554bf909685290ad06783a2b004
IJuanTM <iwan2510@gmail.com>
2023-12-20 23:37:20 +0100

    Added download for version 1.1.1

--- app/Config/app.php
+++ app/Config/app.php
@@ -14,6 +14,6 @@ const REDIRECT = 'home';
 const ERROR_AUTO_REDIRECT = true;
 
 // Simpl information
-const SIMPL_VERSION = '1.1.0';
-const SIMPL_SUMMARY = 'PHP 8.3.0 support, remember me functionality, and more!';
+const SIMPL_VERSION = '1.1.1';
+const SIMPL_SUMMARY = 'Quick update to PHP 8.3.1';
 const SIMPL_RELEASE_DATE = '2023-12-20';
